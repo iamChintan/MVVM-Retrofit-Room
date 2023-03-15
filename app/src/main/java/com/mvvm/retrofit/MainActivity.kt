bc9a76e0cca2413f7cefbe2f045a12605778b48e
CHINTAN LIMBANI <climbani16@gmail.com>
2023-03-15 00:51:27 -0400

    adaptor binding
    not able to bind adaptor in the recyclerview

--- app/src/main/java/com/mvvm/retrofit/MainActivity.kt
+++ app/src/main/java/com/mvvm/retrofit/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.mvvm.retrofit.adaptor.ProgrammingAdaptor
 import com.mvvm.retrofit.api.IQuoteService
 import com.mvvm.retrofit.api.RetrofitHelper
+import com.mvvm.retrofit.databinding.ActivityMainBinding
 import com.mvvm.retrofit.repository.QuoteRepository
 import com.mvvm.retrofit.viewModels.MainViewModel
 import com.mvvm.retrofit.viewModels.MainViewModelFactory
@@ -21,15 +23,18 @@ private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     lateinit var mainViewModel : MainViewModel
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.lifecycleOwner = this
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val adaptor = ProgrammingAdaptor();
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
-        recyclerView.adapter = adaptor
+        binding.adaptor = adaptor
 
         val repository = (application as QuoteApplication).quoteRepository
 
@@ -37,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         
         mainViewModel.quote.observe(this, Observer {
             Toast.makeText(this@MainActivity, it.results.size.toString(),Toast.LENGTH_SHORT).show()
-            adaptor.submitList(it.results)
+//            adaptor.submitList(it.results)
         })
 
 
